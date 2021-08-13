0db2f4649ae39c3a9df2320e99b28446fa761b09
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-13 18:32:03 -0400

    working on header

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+	return (
+		<div>
+			<Head>
+				<title>Amazon Clone</title>
+			</Head>
+
+			<h1>YO</h1>
+			<Header />
+		</div>
+	);
+}
