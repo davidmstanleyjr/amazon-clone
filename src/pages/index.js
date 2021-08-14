c41284449d72a3699448dbaf7f990e956efb12c0
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-14 18:20:24 -0400

    started working on banner.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,12 @@ export default function Home() {
 			</Head>
 
 			<Header />
+			<main className="max-w-screen-2xl nx-auto">
+				{/* Banner */}
+				<Banner />
+
+				{/* Product Feed */}
+			</main>
 		</div>
 	);
 }
