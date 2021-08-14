4ffa155eebc4f262b6d984e96f4bacf4803f3f75
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-14 18:51:20 -0400

    more banner work

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,34 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 function Banner() {
-	return <div></div>;
+	return (
+		<div className="relative">
+			<div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent z-20 bottom-0" />
+			<Carousel
+				// allows it to scroll by itself
+				autoPlay
+				//when it get to the 3rd carousel, it loops back around
+				infiniteLoop
+				showStatus={false}
+				showIndicators={false}
+				showThumbs={false}
+				//5 seconds
+				interval={5000}
+			>
+				{/* Divs represent each carousel on the page */}
+				<div>
+					<img loading="lazy" src="https://links.papareact.com/gi1" />
+				</div>
+				<div>
+					<img loading="lazy" src="https://links.papareact.com/6ff" />
+				</div>
+				<div>
+					<img loading="lazy" src="https://links.papareact.com/7ma" />
+				</div>
+			</Carousel>
+		</div>
+	);
 }
 
 export default Banner;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,39 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
+import ProductFeed from "../components/ProductFeed";
+import { getSession, useSession } from "next-auth/client";
 
-export default function Home() {
+export default function Home({ products }) {
+	const [session, loading] = useSession();
+
 	return (
-		<div>
+		<div className="bg-gray-100">
 			<Head>
 				<title>Amazon Clone</title>
 			</Head>
 
 			<Header />
-			<main className="max-w-screen-2xl nx-auto">
-				{/* Banner */}
+
+			<main className="max-w-screen-2xl mx-auto">
 				<Banner />
 
-				{/* Product Feed */}
+				<ProductFeed products={products} />
 			</main>
 		</div>
 	);
+}
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+	const products = await fetch("https://fakestoreapi.com/products").then(
+		(res) => res.json()
+	);
+
+	return {
+		props: {
+			session,
+			products,
+		},
+	};
 }
