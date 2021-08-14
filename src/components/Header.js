9cf194e9c0f401de43e148a973949b4c7f38ef30
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-14 18:15:16 -0400

    worked some more on header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
-import Image from "next/image";
 import {
 	MenuIcon,
 	SearchIcon,
 	ShoppingCartIcon,
 } from "@heroicons/react/outline";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import { selectItems } from "../slices/basketSlice";
+import { useRouter } from "next/router";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 function Header() {
+	const items = useSelector(selectItems);
+	const router = useRouter();
+	const [session] = useSession();
+
 	return (
-		<header>
-			{/* Top Nav */}
-			<div className="flex-items-center bg-amazon_blue p-1 flex-grow py-2">
+		<header className="sticky top-0 z-50">
+			<div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
 				<div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
 					<Image
+						onClick={() => router.push("/")}
 						src="https://links.papareact.com/f90"
 						width={150}
 						height={40}
@@ -30,20 +38,56 @@ function Header() {
 				</div>
 
 				{/* Right */}
-				<div className="text-white">
-					<div>
-						<p>Hello David Stanley, Jr.</p>
-						<p>Account & Lists</p>
+				<div className="flex items-center text-xs text-white space-x-6 mx-6 whitespace-nowrap">
+					<div
+						onClick={!session ? signIn : signOut}
+						className="cursor-pointer link"
+					>
+						<p className="hover:underline">
+							{session ? `Hello, ${session.user.name}!` : "Sign In"}
+						</p>
+						<p className="font-extrabold md:text-sm">Account & Lists</p>
 					</div>
-					<div>
+
+					<div
+						onClick={() => session && router.push("/orders")}
+						className="cursor-pointer link"
+					>
 						<p>Returns</p>
-						<p>& Orders</p>
+						<p className="font-extrabold md:text-sm">& Orders</p>
 					</div>
-					<div></div>
+
+					<div
+						onClick={() => router.push("/checkout")}
+						className="relative flex items-center cursor-pointer link"
+					>
+						<span className="absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold">
+							{items.length}
+						</span>
+
+						<ShoppingCartIcon className="h-10" />
+						<p className="hidden md:inline font-extrabold md:text-sm mt-2">
+							Basket
+						</p>
+					</div>
 				</div>
 			</div>
-			{/* Bottom Nav */}
-			<div></div>
+
+			{/* Bottom navbar*/}
+			<div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+				<p className="link flex items-center">
+					<MenuIcon className="h-6 mr-1" /> All
+				</p>
+				<p className="link">Prime Video</p>
+				<p className="link">Amazon Business</p>
+				<p className="link">Today's Deals</p>
+				<p className="link hidden lg:inline-flex">Electronics</p>
+				<p className="link hidden lg:inline-flex">Food & Grocery</p>
+				<p className="link hidden lg:inline-flex">Prime</p>
+				<p className="link hidden lg:inline-flex">Buy Again</p>
+				<p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+				<p className="link hidden lg:inline-flex">Health & Personal Care</p>
+			</div>
 		</header>
 	);
 }
